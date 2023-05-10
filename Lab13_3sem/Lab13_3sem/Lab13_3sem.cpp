b0f760e46f4834ce7585fecaabd352ce2c389d5c
MShverdiakov <94805834+MShverdiakov@users.noreply.github.com>
2023-05-10 19:13:06 +0300

    Add files via upload

---
+++ Lab13_3sem/Lab13_3sem/Lab13_3sem.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	string inputS, outputS, s = "please change me";
+	cout << s << endl;
+
+	cout << "Введите подстроку которую нужно заменить -- "; cin >> inputS;
+
+	int index = s.find(inputS);
+	if (index == -1) { cout << "нет такой подстроки"; return 0; }
+
+	cout << "Введите подстроку на которую нужно заменить -- "; cin >> outputS;
+
+	s.replace(index, size(inputS), outputS);
+
+	cout << s;
+}
